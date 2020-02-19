d3d4913a151c5d03b84dc305dd94eaafe37563dd
a.tiurin <a.tiurin@corp.vk.com>
2020-02-19 12:56:52 +0300

    Idling beatify

--- app/src/main/java/com/atiurin/espressoguide/idlingresources/AbstractIdlingResource.kt
+++ app/src/main/java/com/atiurin/espressoguide/idlingresources/AbstractIdlingResource.kt
@@ -1,7 +1,6 @@
 package com.atiurin.espressoguide.idlingresources
 
 import androidx.annotation.Nullable
-import androidx.annotation.VisibleForTesting
 import androidx.test.espresso.IdlingResource
 import java.util.concurrent.atomic.AtomicBoolean
 import androidx.test.espresso.IdlingResource.ResourceCallback
@@ -9,24 +8,24 @@ import androidx.test.espresso.IdlingResource.ResourceCallback
 abstract class AbstractIdlingResource : IdlingResource {
     @Nullable
     @Volatile
-    private var mCallback: ResourceCallback? = null
-    private val mIsIdleNow = AtomicBoolean(true)
+    private var resourceCallback: ResourceCallback? = null
+    private val idleNow = AtomicBoolean(true)
     override fun getName(): String {
         return this.javaClass.name
     }
 
     override fun isIdleNow(): Boolean {
-        return mIsIdleNow.get()
+        return idleNow.get()
     }
 
-    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback?) {
-        mCallback = callback
+    override fun registerIdleTransitionCallback(callback: ResourceCallback?) {
+        resourceCallback = callback
     }
 
     fun setIdleState(isIdleNow: Boolean) {
-        mIsIdleNow.set(isIdleNow)
-        if (isIdleNow && mCallback != null) {
-            mCallback?.onTransitionToIdle()
+        idleNow.set(isIdleNow)
+        if (isIdleNow && resourceCallback != null) {
+            resourceCallback?.onTransitionToIdle()
         }
     }
 }
